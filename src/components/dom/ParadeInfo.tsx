e84389d8e6adc7ec0552f48602e06404101f28bd
ivyroot <nonagoncup@gmail.com>
2022-09-29 12:36:37 -0700

    show parade owner name

--- src/components/dom/ParadeInfo.tsx
+++ src/components/dom/ParadeInfo.tsx
@@ -1,16 +1,20 @@
 import { UseParadeState  } from '@/hooks/UseParadeState'
 import { TokenInfo } from '@/components/dom/TokenInfo'
+import { UseENS } from '@/hooks/UseENS'
 
 export const ParadeInfo = (params) => {
     const address = UseParadeState((state) => state.addressValue)
-    const description = address ? `Token Parade!` : 'Enter an Ethereum address to start NFT parade'
     const isLoading = UseParadeState((state) => state.isLoading)
     const loadingMsg = isLoading ? 'Loading...' : ''
+    const ensLookup = UseENS(address)
+    const paradeOwnerName = (ensLookup && ensLookup.displayName) ? ensLookup.displayName : null
     const handleRestart = () => {
         if (params.onRestart) {
             params.onRestart();
         }
     }
+    const paradeFor = paradeOwnerName ? ` for ${paradeOwnerName}`: ''
+    const description = address ? `Token Parade${paradeFor}!` : 'Enter an Ethereum address to start NFT parade'
     return (
         <div
         className='absolute left-0 max-w-lg px-4 py-2 text-s md:text-base text-gray-50'
