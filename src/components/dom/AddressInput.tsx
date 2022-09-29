aa89fe2f76e653f363a84c8dbe5cebcb9bc61142
ivyroot <nonagoncup@gmail.com>
2022-09-28 23:43:06 -0700

    styling tweaks

--- src/components/dom/AddressInput.tsx
+++ src/components/dom/AddressInput.tsx
@@ -12,14 +12,14 @@ export const AddressInput = () => {
   }
   return (
     <div
-      className='absolute max-w-lg px-4 py-2 text-sm bg-gray-900 shadow-xl md:text-base bottom-8 left-1/2 text-gray-50 transform -translate-x-1/2'
+      className='absolute max-w-lg px-4 py-2 text-sm shadow-xl bg-slate-300 md:text-base bottom-8 left-1/2 text-gray-50 transform -translate-x-1/2'
       style={{
         maxWidth: 'calc(100% - 28px)',
       }}
     >
       <div className='tracking-wider'>
         Enter Address: &nbsp;
-        <input type="text" className='mx-8 text-slate-800 w-96' placeholder='0x123abc456...' value={addressInput} onChange={(e) => {setAddressInput(e.target.value)}} />
+        <input type="text" className='p-2 mx-8 text-sm text-slate-800 w-96' placeholder='0x123abc456...' value={addressInput} onChange={(e) => {setAddressInput(e.target.value)}} />
         <button onClick={startParade}>Go!</button>
       </div>
     </div>
